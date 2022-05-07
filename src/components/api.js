7e91d4db2a0a404996dee3119774a731a8b0a871
hakunamatata93 <ivaschenkodavid@yandex.ru>
2022-05-07 21:28:33 +0300

    api

--- src/components/api.js
+++ src/components/api.js
@@ -1,90 +1,158 @@
-const config = {
-  baseUrl: 'https://nomoreparties.co/v1/plus-cohort-8',
-  headers: {
-    authorization: '69751e77-dec2-4cdf-a14f-2ae5794686d7',
-    'Content-Type': 'application/json'
+export default class Api {
+  constructor(options) {
+    this._options = options
   }
-}
+  _checkResponse(res) {
+    return res.ok ? res.json() : Promise.reject(res)
+  }
+  _getInitialCards() {
+    return fetch(`${this._options.baseUrl}/cards`, {
 
-const onResponse = (res) => {
-  return res.ok ? res.json() : Promise.reject(res)
-}
+             headers: this._options.headers
+        })
+             .then(_checkResponse)
+  }
+  _getInitialProfile(){
+      return fetch(`${this._options.baseUrl}/users/me`, {
+      headers: this._options.headers
+    })
+      .then(_checkResponse)
+  }
 
-export const getMyId = () => {
-  //console.log(config.headers.authorization.replace('-', ''));
-  return config.headers.authorization.replace(/-/g, '');
-}
-export const getInitialCards = () => {
-  return fetch(`${config.baseUrl}/cards`, {
-
-      headers: config.headers
-  })
-      .then(onResponse)
-}
-
-export const getProfile = () => {
-  return fetch(`${config.baseUrl}/users/me`, {
-      headers: config.headers
-  })
-      .then(onResponse)
-}
-
-export const patchProfile = (data) => {
-  return fetch(`${config.baseUrl}/users/me`, {
-      method: 'PATCH',
-      headers: config.headers,
-      body: JSON.stringify(data)
-  })
-      .then(onResponse)
-}
-
-export const postNewCard = (data) => {
-  return fetch(`${config.baseUrl}/cards`, {
+  postNewCard = (name, link) => {
+  return fetch(`${this._options.baseUrl}/cards`, {
       method: 'POST',
-      headers: config.headers,
-      body: JSON.stringify(data)
+      headers: this._options.headers,
+      body: JSON.stringify({
+        name: name,
+        link: link
+      })
   })
-      .then(onResponse)
-
+      .then(_checkResponse)
 }
-export const deleteCard = (id) => {
-  return fetch(`${config.baseUrl}/cards/${id}`, {
+
+  _deleteCard(id){
+  return fetch(`${this._options.baseUrl}/cards/${id}`, {
       method: 'DELETE',
-      headers: config.headers
+      headers: this._options.headers
   })
-      .then(onResponse)
+      .then(_checkResponse)
+  }
 
-}
-
-export const deleteLike = (id) => {
-  return fetch(`${config.baseUrl}/cards/likes/${id}`, {
+  _deleteLike(id){
+    return fetch(`${this._options.baseUrl}/cards/likes/${id}`, {
       method: 'DELETE',
-      headers: config.headers
+      headers: this._options.headers
   })
-      .then(onResponse)
+      .then(_checkResponse)
+  }
 
-}
-
-
-export const putLike = (id) => {
-  return fetch(`${config.baseUrl}/cards/likes/${id}`, {
+  _putLike(id){
+    return fetch(`${this._options.baseUrl}/cards/likes/${id}`, {
       method: 'PUT',
-      headers: config.headers
+      headers: this._options.headers
   })
-      .then(onResponse)
+      .then(_checkResponse)
+  }
 
-}
-
-export const patchAvatar = (url) => {
-  return fetch(`${config.baseUrl}/users/me/avatar`, {
+  _patchAvatar(url){
+    return fetch(`${this._options.baseUrl}/users/me/avatar`, {
       method: 'PATCH',
-      headers: config.headers,
+      headers: this._options.headers,
       body: JSON.stringify({ avatar: url })
-
-  })
-      .then(onResponse)
+    })
+    .then(_checkResponse)
+  }
 
 }
+
+// const config = {
+//   baseUrl: 'https://nomoreparties.co/v1/plus-cohort-8',
+//   headers: {
+//     authorization: '69751e77-dec2-4cdf-a14f-2ae5794686d7',
+//     'Content-Type': 'application/json'
+//   }
+// }
+
+// const onResponse = (res) => {
+//   return res.ok ? res.json() : Promise.reject(res)
+// }
+
+// export const getMyId = () => {
+//   //console.log(config.headers.authorization.replace('-', ''));
+//   return config.headers.authorization.replace(/-/g, '');
+// }
+// export const getInitialCards = () => {
+//   return fetch(`${config.baseUrl}/cards`, {
+
+//       headers: config.headers
+//   })
+//       .then(onResponse)
+// }
+
+// export const getProfile = () => {
+//   return fetch(`${config.baseUrl}/users/me`, {
+//       headers: config.headers
+//   })
+//       .then(onResponse)
+// }
+
+// export const patchProfile = (data) => {
+//   return fetch(`${config.baseUrl}/users/me`, {
+//       method: 'PATCH',
+//       headers: config.headers,
+//       body: JSON.stringify(data)
+//   })
+//       .then(onResponse)
+// }
+
+// export const postNewCard = (data) => {
+//   return fetch(`${config.baseUrl}/cards`, {
+//       method: 'POST',
+//       headers: config.headers,
+//       body: JSON.stringify(data)
+//   })
+//       .then(onResponse)
+
+// }
+// export const deleteCard = (id) => {
+//   return fetch(`${config.baseUrl}/cards/${id}`, {
+//       method: 'DELETE',
+//       headers: config.headers
+//   })
+//       .then(onResponse)
+
+// }
+
+// export const deleteLike = (id) => {
+//   return fetch(`${config.baseUrl}/cards/likes/${id}`, {
+//       method: 'DELETE',
+//       headers: config.headers
+//   })
+//       .then(onResponse)
+
+// }
+
+
+// export const putLike = (id) => {
+//   return fetch(`${config.baseUrl}/cards/likes/${id}`, {
+//       method: 'PUT',
+//       headers: config.headers
+//   })
+//       .then(onResponse)
+
+// }
+
+// export const patchAvatar = (url) => {
+//   return fetch(`${config.baseUrl}/users/me/avatar`, {
+//       method: 'PATCH',
+//       headers: config.headers,
+//       body: JSON.stringify({ avatar: url })
+
+//   })
+//       .then(onResponse)
+
+// }
 
 
 
---
+++ src/components/popupWithForm.js
@@ -0,0 +1,5 @@
+import Popup from './popup.js'
+
+export default class PopupWithForm extends Popup{
+
+}
