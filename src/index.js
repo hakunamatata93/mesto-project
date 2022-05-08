97bd99b004ce1c38c4de1a18ec695b4cdfb901d7
hakunamatata93 <ivaschenkodavid@yandex.ru>
2022-05-08 18:01:45 +0300

    edit profile

--- src/index.js
+++ src/index.js
@@ -8,54 +8,77 @@ import PopupWithImage from './components/PopupWithImage';
 import Validator from './components/Validator';
 import {startLoading, stopLoading} from './components/utils';
 import {
-  config,
-  editProfileButton,
-  addButton,
-  editAvatar,
-  nameInput,
-  jobInput,
-  validationConfig,
-  userSelectors,
-  formSelectors
+    config,
+    editProfileButton,
+    addButton,
+    editAvatar,
+    nameInput,
+    jobInput,
+    validationConfig,
+    userSelectors,
+    formSelectors
 } from './components/constants';
 
 //экземпляры
 const api = new Api(config);
 
 const userInfo = new UserInfo(
-  userSelectors.profileName,
-  userSelectors.profileJob,
-  userSelectors.ProfileAvatar
+    userSelectors.profileName,
+    userSelectors.profileJob,
+    userSelectors.ProfileAvatar
 );
 //валидация
 const editProfilealidator = new Validator(
-  validationConfig,
-  formSelectors.editProfileForm
+    validationConfig,
+    formSelectors.editProfileForm
 );
 const addCardValidator = new Validator(
-  validationConfig,
-  formSelectors.addCardForm
+    validationConfig,
+    formSelectors.addCardForm
 );
 const editAvatarValidator = new Validator(
-  validationConfig,
-  formSelectors.editAvatarForm
+    validationConfig,
+    formSelectors.editAvatarForm
 );
 editProfilealidator.enableValidation();
 addCardValidator.enableValidation();
 editAvatarValidator.enableValidation();
 
 const cardList = new Section({
-  renderer:(item) =>{
-    const card = new Card({
-      data: item,
-      handleCardClick,
-      handleDeleteClick,
-      toggleLike,
-      selector: '#elements__template',
-      userId: userInfo.getUserInfo().userId
-    });
+    renderer:(item) =>{
+        const card = new Card({
+            data: item,
+            handleCardClick,
+            handleDeleteClick,
+            toggleLike,
+            selector: '#elements__template',
+            userId: userInfo.getUserInfo().userId
+        });
     return card.generate();
-  },
+    },
 },
 '.elements__list'
 );
+
+const bigPicturePopup = new PopupWithImage('.popup__big-picture');
+bigPicturePopup.setEventListeners();
+
+const editProfilePopup = new PopupWithForm({
+  popupSelector:'.popup__profile',
+  handleFormSubmit:(data)=>{
+    startLoading('popup__profile'),
+    api.patchProfile(data.profileName, data.profileJob)
+    .then(userData =>{
+      userInfo.setUserInfo(userData);
+      editProfilePopup.close();
+    })
+    .catch((err)=>{
+      console.log(`Error: ${err}`);
+    })
+    .finally(()=>{
+      stopLoading('.popup__profile')
+    })
+  }
+})
+
+
