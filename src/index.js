c4ba0357a7ee640006a7cce27c953fc2a36f66e1
hakunamatata93 <ivaschenkodavid@yandex.ru>
2022-05-08 17:14:26 +0300

    import constants

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-
 import './styles/index.css';
-import * as cards from './components/cards';
-import * as validator from './components/validate';
-export let userId;
-cards.initialize();
-validator.enableValidation(validator.validationConfig);
+import Api from './components/Api';
+import UserInfo from './components/UserInfo';
+import Card from './components/Card';
+import Section from './components/Section';
+import PopupWithForm from './components/popupWithForm';
+import PopupWithImage from './components/PopupWithImage';
+import Validator from './components/Validator';
+import {startLoading, stopLoading} from './components/utils';
+import {
+  config,
+  editProfileButton,
+  addButton,
+  editAvatar,
+  nameInput,
+  jobInput,
+  validationConfig,
+  userSelectors,
+  formSelectors
+} from './components/constants';
 
+//экземпляры
+const api = new Api(config);
+const userInfo = new UserInfo(config);
+const card = new Card(config);
+
+const section = new Section(config);
+const popupWithForm = new PopupWithForm(config);
+const popupWithImage = new PopupWithImage(config);
+const validator = new Validator(config);
 
