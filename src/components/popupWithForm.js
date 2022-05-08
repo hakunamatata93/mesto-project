7e8f74427a3fbfd6dfe8a9d7fb4f1b031bf67bdf
edjme <stasikparmut@yandex.ru>
2022-05-08 16:47:34 +0300

    PopupWithForm.js

--- src/components/popupWithForm.js
+++ src/components/popupWithForm.js
@@ -1,5 +1,34 @@
 import Popup from './Popup.js'
 
-export default class PopupWithForm extends Popup{
+export default class PopupWithForm extends Popup {
+    constructor({ popupSelector, handleFormSubmit }) {
 
+    super(popupSelector);
+    this._form = this._popup.querySelector('.popup');
+    this._handleFormSubmit = handleFormSubmit;
+    this._inputList = this._form.querySelectorAll('.popup__input');
+    }
+
+    close() {
+        super.close();
+        this._form.reset();
+    }
+
+    _getInputValues() {
+        this._formValues = {};
+        this._inputList.forEach(input => {
+        this._formValues[input.name] = input.value;
+        })
+
+        return this._formValues;
+    }
+
+    setEventListeners() {
+        super.setEventListeners();
+        this._form.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+            this._getInputValues();
+            this._handleFormSubmit(this._getInputValues());
+        })
+    }
 }
